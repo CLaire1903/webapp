a207dcf0f1a2d8ef7c453f59c6ad6a8134d3eb14
Tang Jia Huey <tangjiahuey6738@e.newera.edu.my>
2021-05-25 10:52:30 +0800

    done exercise 2

--- week2/exercise1.php
+++ week2/exercise1.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html>
+
 <body>
 
-<?php
-echo "My name is Claire.";
-?>
-<?php
-echo "My student ID is 1930036-BSE.";
-?>
+    <?php
+    echo "My name is Claire.<br>";
+    ?>
+    <?php
+    echo "My student ID is 1930036-BSE.";
+    ?>
 
 </body>
+
 </html>
---
+++ week2/exercise2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+    $color = "pink";
+    $number = "9";
+    $name = "Claire";
+    echo "My favourite colour is " . $color . ".<br>";
+    echo "My favourite number is " . $number . ".<br>";
+    echo "My name is " . $name . ".<br>";
+    ?>
+</body>
+
+</html>
