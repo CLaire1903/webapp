b3289f289b883250e4f2afce1d8a2165c832c5a4
Tang Jia Huey <tangjiahuey6738@e.newera.edu.my>
2021-05-25 12:39:44 +0800

    exercise 6 edited

--- week2/exercise6.php
+++ week2/exercise6.php
@@ -2,23 +2,43 @@
 <html>
 
 <head>
-    <title>Exercise 6 - Compare between two random numbers by using different colour.</title>
+    <title>Exercise 6 - Compare between two random numbers by using css.</title>
 </head>
+<style>
+    .container {
+        display: flex;
+    }
+
+    .bigger {
+        font-weight: bold;
+        color: red;
+        padding: 10px;
+    }
+
+    .smaller {
+        color: blue;
+        padding: 10px;
+    }
+</style>
 
 <body>
     <?php
     $number1 = rand(10, 100);
     $number2 = rand(10, 100);
 
-    echo "Number 1 = " .$number1. "<br>";
-    echo "Number 2 = " .$number2. "<br>";
+    echo "Number 1 = " . $number1 . "<br>";
+    echo "Number 2 = " . $number2 . "<br>";
 
     if ($number1 > $number2) {
-        echo "<font color = 'red'> <strong> $number1 </strong> </font>";
-        echo "<font color = 'blue'> $number2 </font>";
+        echo " <div class = container>";
+        echo " <div class = bigger> $number1 </div>";
+        echo " <div class = smaller> $number2 </div>";
+        echo " </div>";
     } else {
-        echo "<font color = 'blue'> $number1 </font>";
-        echo "<font color = 'red'> <strong> $number2 </strong> </font>";
+        echo " <div class = container>";
+        echo " <div class = smaller> $number1 </div>";
+        echo " <div class = bigger> $number2 </div>";
+        echo " </div>";
     }
 
     ?>
