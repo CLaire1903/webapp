f76f8ab9edd4b31eef99da85ef7b946f3fb16145
Tang Jia Huey <tangjiahuey6738@e.newera.edu.my>
2021-06-06 18:46:19 +0800

    product edited

--- online_store/product.php
+++ online_store/product.php
@@ -8,47 +8,46 @@
 
 <body>
     <div class="container">
-    <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
-    <div class="container-fluid">
-        <h3 class="text-light">Claire_Store</h3>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-around" id="navbarToggle"> 
-            <div>
-                <ul class="navbar-nav">
-                    <li class="nav-item ">
-                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="product.php">Create Product</a>
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="customer.php">Create Customer</a>
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact.php">Contact Us</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-</nav>
+        <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
+            <div class="container-fluid">
+                <h3 class="text-light">Claire_Store</h3>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-around" id="navbarToggle">
+                    <div>
+                        <ul class="navbar-nav">
+                            <li class="nav-item ">
+                                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="product.php">Create Product</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="customer.php">Create Customer</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="contact.php">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+        </nav>
         <div class="page-header">
             <h4 class="p-1">Create Product</h4>
         </div>
-
         <?php
         if ($_POST) {
             include 'config/database.php';
@@ -76,8 +75,7 @@
                 } else {
                     echo "<div class='alert alert-danger'>Unable to save record.</div>";
                 }
-            }
-            catch (PDOException $exception) {
+            } catch (PDOException $exception) {
                 die('ERROR: ' . $exception->getMessage());
             }
         }
