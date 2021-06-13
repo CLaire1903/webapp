23289379a0fa1b88cf1dc375662fa5219c12be5c
Tang Jia Huey <tangjiahuey6738@e.newera.edu.my>
2021-06-13 22:36:24 +0800

    customer validation done

--- online_store/customer.php
+++ online_store/customer.php
@@ -50,32 +50,65 @@
         </div>
         <?php
         if ($_POST) {
-            if ($_POST['username'] != "" &&  $_POST['password'] != "" &&  $_POST['firstName'] != "" &&  $_POST['lastName'] != "" &&  $_POST['gender'] != "" &&  $_POST['dateOfBirth'] != "" &&  $_POST['accountStatus']) {
-                include 'config/database.php';
-                try {
-                    $query = "INSERT INTO customers SET username=:username, password=:password, firstName=:firstName, lastName=:lastName, gender=:gender, dateOfBirth=:dateOfBirth, accountStatus=:accountStatus";
-                    $stmt = $con->prepare($query);
-                    $username = $_POST['username'];
-                    $password = $_POST['password'];
-                    $firstName = $_POST['firstName'];
-                    $lastName = $_POST['lastName'];
-                    $gender = $_POST['gender'];
-                    $dateOfBirth = $_POST['dateOfBirth'];
-                    $accountStatus = $_POST['accountStatus'];
-                    $stmt->bindParam(':username', $username);
-                    $stmt->bindParam(':password', $password);
-                    $stmt->bindParam(':firstName', $firstName);
-                    $stmt->bindParam(':lastName', $lastName);
-                    $stmt->bindParam(':gender', $gender);
-                    $stmt->bindParam(':dateOfBirth', $dateOfBirth);
-                    $stmt->bindParam(':accountStatus', $accountStatus);
-                    if ($stmt->execute()) {
-                        echo "<div class='alert alert-success'>Record was saved.</div>";
+            if ($_POST['username'] != "" &&  $_POST['password'] != "" &&  $_POST['confirmPassword'] != "" &&  $_POST['firstName'] != "" &&  $_POST['lastName'] != "" &&  $_POST['gender'] != "" &&  $_POST['dateOfBirth'] != "" &&  $_POST['accountStatus']) {
+                if (strlen($_POST['username']) >= 6 && (strrpos($_POST['username'], " ") == false)) {
+                    if ($_POST['password'] == $_POST['confirmPassword']) {
+                        if (strlen($_POST['password']) >= 8) {
+                            if (preg_match("@[0-9]@", $_POST['password'])) {
+                                if (preg_match("@[a-z]@", $_POST['password'])) {
+                                    if (preg_match("@[A-Z]@", $_POST['password'])) {
+                                        $today = date('Y-M-D');
+                                        if ($today - $_POST['dateOfBirth'] >= 18) {
+                                            include 'config/database.php';
+                                            try {
+                                                $query = "INSERT INTO customers SET username=:username, password=:password,confirmPassword=:confirmPassword, firstName=:firstName, lastName=:lastName, gender=:gender, dateOfBirth=:dateOfBirth, accountStatus=:accountStatus";
+                                                $stmt = $con->prepare($query);
+                                                $username = $_POST['username'];
+                                                $password = $_POST['password'];
+                                                $confirmPassword = $_POST['confirmPassword'];
+                                                $firstName = $_POST['firstName'];
+                                                $lastName = $_POST['lastName'];
+                                                $gender = $_POST['gender'];
+                                                $dateOfBirth = $_POST['dateOfBirth'];
+                                                $accountStatus = $_POST['accountStatus'];
+                                                $stmt->bindParam(':username', $username);
+                                                $stmt->bindParam(':password', $password);
+                                                $stmt->bindParam(':confirmPassword', $confirmPassword);
+                                                $stmt->bindParam(':firstName', $firstName);
+                                                $stmt->bindParam(':lastName', $lastName);
+                                                $stmt->bindParam(':gender', $gender);
+                                                $stmt->bindParam(':dateOfBirth', $dateOfBirth);
+                                                $stmt->bindParam(':accountStatus', $accountStatus);
+                                                if ($stmt->execute()) {
+                                                    echo "<div class='alert alert-success'>Record was saved.</div>";
+                                                } else {
+                                                    echo "<div class='alert alert-danger'>Unable to save record.</div>";
+                                                }
+                                            } catch (PDOException $exception) {
+                                                die('ERROR: ' . $exception->getMessage());
+                                            }
+                                        } else {
+                                            echo "<div class='alert alert-danger'> User must be 18 years old and above. </div>";
+                                        }
+                                    } else {
+                                        echo "<div class='alert alert-danger'> Passowrd must 
+                            contain at least a<strong> CAPITAL </strong>letter. </div>";
+                                    }
+                                } else {
+                                    echo "<div class='alert alert-danger'> Passowrd must 
+                        contain at least a <strong>SMALL</strong> letter. </div>";
+                                }
+                            } else {
+                                echo "<div class='alert alert-danger'> Passowrd must contain at least a number. </div>";
+                            }
+                        } else {
+                            echo "<div class='alert alert-danger'> Password should be at least 8 character.</div>";
+                        }
                     } else {
-                        echo "<div class='alert alert-danger'>Unable to save record.</div>";
+                        echo "<div class='alert alert-danger'> Password and confirm password are not the same.</div>";
                     }
-                } catch (PDOException $exception) {
-                    die('ERROR: ' . $exception->getMessage());
+                } else {
+                    echo "<div class='alert alert-danger'>Username must be at least 6 characters and no space included.</div>";
                 }
             } else {
                 echo "<div class='alert alert-danger'>Make sure all fields are not empty</div>";
@@ -91,6 +124,10 @@
                 <tr>
                     <td>Password</td>
                     <td><input type='password' name='password' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Confirm Password</td>
+                    <td><input type='password' name='confirmPassword' class='form-control' /></td>
                 </tr>
                 <tr>
                     <td>First Name</td>
@@ -139,7 +176,7 @@
                     <td></td>
                     <td>
                         <input type='submit' value='Save' class='btn btn-primary' />
-                        <a href='index.php' class='btn btn-danger'>Back to read products</a>
+                        <a href='index.php' class='btn btn-danger'>Back</a>
                     </td>
                 </tr>
             </table>
