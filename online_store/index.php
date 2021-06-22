c91a7d22674a149add59468cf5632e35f95b234e
Tang Jia Huey <tangjiahuey6738@e.newera.edu.my>
2021-06-22 08:52:13 +0800

    order, order list and order detail done

--- online_store/index.php
+++ online_store/index.php
@@ -11,31 +11,18 @@
         <?php
         include 'navigation.php';
         ?>
-        <div class="contain d-flex justify-content-center" style="background-image: url('background.jpg'); background-size:cover; height:500px;">
+        <div class="contain d-flex justify-content-center" style="background-image: url('image/background.jpg'); background-size:cover; height:500px;">
             <div>
                 <h1 class="text-light p-5">
                     Welcome to Claire's Online Store.
                 </h1>
             </div>
         </div>
-        <div id="footer" class=" justify-content-between">
-                    <div class="iconLink">
-                        <a class="nav-link" href="https://www.facebook.com/"><img src="image/facebook.png" alt="Facebook"></a>
-                    </div>
-                    <div class="iconLink">
-                        <a class="nav-link" href="https://www.instagram.com/"><img src="image/instagram.png" alt="Instagram"></a>
-                    </div>
-                    <div class="iconLink">
-                        <a class="nav-link" href="#"><img src="image/whatsapp.png" alt="Whatsapp"></a>
-                    </div>
-                    <div class="iconLink">
-                        <a class="nav-link" href="#"><img src="image/email.png" alt="Email"></a>
-                    </div>
-                    <div class="iconLink">
-                        <a class="nav-link" href="#"><img src="image/wechat.png" alt="Wechat"></a>
-                    </div>
-            </div>
-
+        <div class="footer bg-dark">
+            <?php
+            include 'footer.php';
+            ?>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
