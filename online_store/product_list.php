6b23883b68ebbd9062814e154a53bd52aad2dd79
Tang Jia Huey <tangjiahuey6738@e.newera.edu.my>
2021-07-21 23:08:12 +0800

    product_list and order_list edited

--- online_store/product_list.php
+++ online_store/product_list.php
@@ -33,97 +33,13 @@ if (!isset($_SESSION["cus_username"])) {
         ?>
 
         <div>
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" onsubmit="return validation()" method="post">
-                <table class='table table-hover table-responsive table-bordered' style="border:none;">
-                    <tr class='searchProduct' style="border:none;">
-                        <td class="col-9" style="border:none;"><input type='text' name='search' id="search" placeholder='Search products' class='form-control'></td>
-                        <td style="border:none;"><input type='submit' value='Search' class='btn btn-secondary '></td>
-                        <td style="border:none;"><a href='product.php' class='btn btn-secondary'>Create New Product</a></td>
-                    </tr>
-                </table>
-            </form>
+            <table class='table table-hover table-responsive table-bordered' style="border:none;">
+                <tr class='searchProduct' style="border:none;">
+                    <td class="col-10" style="border:none;"><input type='text' name='search' id="search" onkeyup="myFunction()" placeholder='Search products' class='form-control'></td>
+                    <td style="border:none;"><a href='product.php' class='btn btn-primary'>Create New Product</a></td>
+                </tr>
+            </table>
         </div>
-
-        <?php
-        if (isset($_POST['search'])) {
-            try {
-                if (empty($_POST['search'])) {
-                    throw new Exception("Please enter the keywords!");
-                }
-                $searchq = $_POST['search'];
-                $searchq = preg_replace("#[^0-9a-z]#i", "", $searchq);
-                $searchProductQuery = "SELECT * FROM products WHERE name LIKE '%$searchq%' OR name_malay LIKE '%$searchq%'";
-                $searchProductStmt = $con->prepare($searchProductQuery);
-                $searchProductStmt->execute();
-                $count = $searchProductStmt->rowCount();;
-                if ($count == 0) {
-                    echo "<h5>There was no search results!</h5>";
-                    $query = "SELECT productID, name, description, price FROM products ORDER BY productID DESC";
-                    $stmt = $con->prepare($query);
-                    $stmt->execute();
-                    $num = $stmt->rowCount();
-
-                    if ($num > 0) {
-                        echo "<table class='table table-hover table-responsive table-bordered'>";
-
-                        echo "<tr>";
-                        echo "<th>ID</th>";
-                        echo "<th>Name</th>";
-                        echo "<th>Description</th>";
-                        echo "<th>Price</th>";
-                        echo "<th>Action</th>";
-                        echo "</tr>";
-
-                        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                            extract($row);
-                            echo "<tr>";
-                            echo "<td>{$productID}</td>";
-                            echo "<td>{$name}</td>";
-                            echo "<td>{$description}</td>";
-                            echo "<td>{$price}</td>";
-                            echo "<td>";
-                            echo "<a href='product_detail.php?productID={$productID}' class='btn btn-info me-2'>Detail</a>";
-                            echo "<a href='product_update.php?productID={$productID}' class='btn btn-primary me-2'>Edit</a>";
-                            echo "<a href='#' onclick='delete_product({$productID});'  class='btn btn-danger'>Delete</a>";
-                            echo "</td>";
-                            echo "</tr>";
-                        }
-                        echo "</table>";
-                    } else {
-                        echo "<div class='alert alert-danger'>No records found.</div>";
-                    }
-                } else {
-                    echo "<table class='table table-hover table-responsive table-bordered'>";
-                    echo "<tr>";
-                    echo "<th>ID</th>";
-                    echo "<th>Name</th>";
-                    echo "<th>Description</th>";
-                    echo "<th>Price</th>";
-                    echo "<th>Action</th>";
-                    echo "</tr>";
-                    while ($row = $searchProductStmt->fetch(PDO::FETCH_ASSOC)) {
-                        extract($row);
-                        echo "<tr>";
-                        echo "<td>{$productID}</td>";
-                        echo "<td>{$name}</td>";
-                        echo "<td class='col-7'>{$description}</td>";
-                        echo "<td>{$price}</td>";
-                        echo "<td>";
-                        echo "<a href='product_detail.php?productID={$productID}' class='btn btn-info me-2'>Detail</a>";
-                        echo "<a href='product_update.php?productID={$productID}' class='btn btn-primary me-2'>Edit</a>";
-                        echo "<a href='#' onclick='delete_product({$productID});'  class='btn btn-danger'>Delete</a>";
-                        echo "</td>";
-                        echo "</tr>";
-                    }
-                }
-            } catch (PDOException $exception) {
-                //for databae 'PDO'
-                echo "<div class='alert alert-danger'>" . $exception->getMessage() . "</div>";
-            } catch (Exception $exception) {
-                echo "<div class='alert alert-danger'>" . $exception->getMessage() . "</div>";
-            }
-        }
-        ?>
 
         <?php
         $query = "SELECT productID, name, description, price FROM products ORDER BY productID DESC";
@@ -132,12 +48,12 @@ if (!isset($_SESSION["cus_username"])) {
         $num = $stmt->rowCount();
 
         if ($num > 0) {
-            echo "<table id='oriTable' class='table table-hover table-responsive table-bordered'>";
+            echo "<table id='myTable' class='table table-hover table-responsive table-bordered'>";
 
             echo "<tr>";
             echo "<th>ID</th>";
             echo "<th>Name</th>";
-            echo "<th>Description</th>";
+            echo "<th class='col-7'>Description</th>";
             echo "<th>Price</th>";
             echo "<th>Action</th>";
             echo "</tr>";
@@ -171,22 +87,22 @@ if (!isset($_SESSION["cus_username"])) {
             }
         }
 
-        function validation() {
-            var oriTable = document.getElementById("oriTable");
-            var search = document.getElementById("search").value;
-            var flag = false;
-            var msg = "";
-            if (search == '') {
-                flag = true;
-                msg = msg + "Please enter the keywords!\r\n";
-            }else{ 
-                oriTable.style.display="none";
-            }
-            if (flag == true) {
-                alert(msg);
-                return false;
-            } else {
-                return true;
+        function myFunction() {
+            var input, filter, table, tr, td, i, txtValue;
+            var input = document.getElementById("search");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("myTable");
+            tr = table.getElementsByTagName("tr");
+            for (i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName("td")[1];
+                if (td) {
+                    txtValue = td.textContent || td.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }
             }
         }
     </script>
--- online_store/testing.php
+++ online_store/testing.php
@@ -20,6 +20,16 @@ if (!isset($_SESSION["cus_username"])) {
             <h1>Order List</h1>
         </div>
 
+        <div>
+            <table class='table table-hover table-responsive table-bordered' style="border:none;">
+                <tr class='searchProduct' style="border:none;">
+                    <td class="col-10" style="border:none;"><input type='text' name='search' id="search" onkeyup="myFunction()" placeholder='Search orders' class='form-control'></td>
+                    <td style="border:none;"><a href='order.php' class='btn btn-primary'>Create New Order</a></td>
+                </tr>
+            </table>
+        </div>
+
+
         <?php
         include 'config/database.php';
         $action = isset($_GET['action']) ? $_GET['action'] : "";
@@ -30,9 +40,8 @@ if (!isset($_SESSION["cus_username"])) {
         $stmt = $con->prepare($query);
         $stmt->execute();
         $num = $stmt->rowCount();
-        echo "<a href='order.php' class='btn btn-primary mb-2'>Create New Order</a>";
         if ($num > 0) {
-            echo "<table class='table table-hover table-responsive table-bordered'>";
+            echo "<table class='table table-hover table-responsive table-bordered' id='myTable'>";
 
             echo "<tr>";
             echo "<th>Order ID</th>";
@@ -65,9 +74,29 @@ if (!isset($_SESSION["cus_username"])) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
     <script type='text/javascript'>
         function delete_order(orderID) {
-
             if (confirm('Are you sure?')) {
                 window.location = 'order_delete.php?orderID=' + orderID;
+            }
+        }
+
+        function myFunction() {
+            var input, filter, table, tr, td, i, txtValue;
+            var input = document.getElementById("search");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("myTable");
+            tr = table.getElementsByTagName("tr");
+            for (i = 0; i < tr.length; i++) {
+                username = tr[i].getElementsByTagName("td")[2];
+                id = tr[i].getElementsByTagName("td")[0];
+                if (username || id) {
+                    usernameCol = username.textContent || username.innerText;
+                    idCol = id.textContent || id.innerText;
+                    if (usernameCol.toUpperCase().indexOf(filter) > -1 || idCol.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }
             }
         }
     </script>
