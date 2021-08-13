375da14892dcf9dd14c0a7f4ec6b9ca1001d1985
Tang Jia Huey <tangjiahuey6738@e.newera.edu.my>
2021-08-14 00:57:07 +0800

    css edited

--- online_store/order_update.php
+++ online_store/order_update.php
@@ -11,6 +11,7 @@ if (!isset($_SESSION["cus_username"])) {
     <title>Homework - Update Order</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link href="css/general.css" rel="stylesheet">
+    <link href="css/order.css" rel="stylesheet">
 
     <style>
         #deleteBtn {
